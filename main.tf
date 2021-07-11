8ec1129c06c441e69230566d719f52dcee6d48bd
msimou <marios.simouu@gmail.com>
2021-07-11 11:46:36 +0100

    feat: setup (#1)

--- main.tf
+++ main.tf
@@ -1,54 +1,15 @@
-resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
-
-    tags = {
-        Name = format("%s-vpc", var.org)
-    }
-}
-
-resource "aws_main_route_table_association" "vpc_rt_association" {
-    vpc_id = aws_vpc.vpc.id
-    route_table_id = aws_vpc.vpc.default_route_table_id
-}
-
-resource "aws_default_route_table" "rt" {
-    default_route_table_id = aws_vpc.vpc.default_route_table_id
-
-    tags = {
-        Name = format("%s-rt", var.org)
-    }
-}
-
-resource "aws_default_network_acl" "network_acl" {
-    default_network_acl_id = aws_vpc.vpc.default_network_acl_id
-
-    tags = {
-        Name = format("%s-network-acl", var.org)
-    }
-}
-
-resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.vpc.id
-
-    tags = {
-        Name = format("%s-igw", var.org)
-    }
-}
-
 locals {
     subnets = [
         {
-            cidr_block = "10.0.1.0/24"
+            cidr_block = "172.31.16.0/20"
             az = "us-east-1a"
         },
         {
-            cidr_block = "10.0.2.0/24"
+            cidr_block = "172.31.32.0/20"
             az = "us-east-1b"
         },
         {
-            cidr_block = "10.0.3.0/24"
+            cidr_block = "172.31.0.0/20"
             az = "us-east-1c"
         }
     ]
@@ -127,6 +88,49 @@ locals {
                     to_port = 8080
                     protocol = "tcp"
                     cidr_blocks = ["0.0.0.0/0"]
+                },
+                {
+                    type = "egress"
+                    from_port = 0
+                    to_port = 0
+                    protocol = "all"
+                    cidr_blocks = ["0.0.0.0/0"]
+                }
+            ]
+        }
+        hello = {
+            rules = [
+                {
+                    type = "ingress"
+                    from_port = 8080
+                    to_port = 8080
+                    protocol = "tcp"
+                    cidr_blocks = ["0.0.0.0/0"]
+                },
+                {
+                    type = "egress"
+                    from_port = 0
+                    to_port = 0
+                    protocol = "all"
+                    cidr_blocks = ["0.0.0.0/0"]
+                }
+            ]
+        }
+        lb = {
+            rules = [
+                {
+                    type = "ingress"
+                    from_port = 80
+                    to_port = 80
+                    protocol = "tcp"
+                    cidr_blocks = ["0.0.0.0/0"]
+                },
+                {
+                    type = "egress"
+                    from_port = 0
+                    to_port = 0
+                    protocol = "all"
+                    cidr_blocks = ["0.0.0.0/0"]
                 }
             ]
         }
@@ -142,12 +146,97 @@ locals {
     )
     security_groups_ids = [for sg in aws_security_group.sgs: sg.id]
     services = {
-        "web" = {}
+        web = {
+            cpu = 256
+            memory = 512
+            essential = true
+            containerPort = 8080
+            desired_count = 1
+            max_count = 3
+            health_path = "/ping"
+            listener_priority = 20
+        }
+        hello = {
+            cpu = 256
+            memory = 512
+            essential = true
+            containerPort = 8080
+            desired_count = 1
+            max_count = 3
+            health_path = "/ping"
+            listener_priority = 10
+        }
     }
     service_target_group_id_map = zipmap(
         [for serviceName, options in local.services: serviceName],
         [for tg in aws_lb_target_group.lb_tgs: tg.id]
     )
+    service_repository_map = zipmap(
+        [for serviceName, options in local.services: serviceName ],
+        [for repo in aws_ecr_repository.ecr_repositories: repo.repository_url]
+    )
+    service_task_definition_map = zipmap(
+        [for serviceName, options in local.services: serviceName],
+        [for taskDefinition in aws_ecs_task_definition.taskDefinitions: taskDefinition.arn]
+    )
+    service_appautoscaling_target_map = zipmap(
+        [for serviceName, options in local.services: serviceName],
+        [for ast in aws_appautoscaling_target.ecs_services_autoscaling_groups: ast ]
+    )
+}
+
+resource "aws_vpc" "vpc" {
+    cidr_block = "172.31.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+
+    tags = {
+        Name = format("%s-vpc", var.org)
+    }
+}
+
+resource "aws_main_route_table_association" "vpc_rt_association" {
+    vpc_id = aws_vpc.vpc.id
+    route_table_id = aws_vpc.vpc.default_route_table_id
+}
+
+resource "aws_default_route_table" "rt" {
+    default_route_table_id = aws_vpc.vpc.default_route_table_id
+
+    tags = {
+        Name = format("%s-rt", var.org)
+    }
+}
+
+resource "aws_default_network_acl" "network_acl" {
+    default_network_acl_id = aws_vpc.vpc.default_network_acl_id
+
+    tags = {
+        Name = format("%s-network-acl", var.org)
+    }
+
+    lifecycle {
+        ignore_changes = [egress, ingress, subnet_ids]
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc.id
+
+    tags = {
+        Name = format("%s-igw", var.org)
+    }
+}
+
+resource "aws_flow_log" "flow_log" {
+    iam_role_arn = aws_iam_role.bot_role.arn
+    log_destination = aws_cloudwatch_log_group.log_group.arn
+    traffic_type = "ALL"
+    vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+    name = format("%s-log-group", var.org)
 }
 
 resource "aws_subnet" "subnets" {
@@ -157,7 +246,7 @@ resource "aws_subnet" "subnets" {
     cidr_block = element(local.subnets, count.index).cidr_block
     availability_zone = element(local.subnets, count.index).az
     tags = {
-        Name = format("%s-subnet", var.org)
+        Name = format("%s-subnet-%s", var.org, count.index)
     }
 }
 
@@ -213,7 +302,7 @@ resource "aws_lb" "lb" {
     internal = false
     load_balancer_type = "application"
     subnets = local.subnets_ids
-    security_groups = local.security_groups_ids
+    security_groups = [ lookup(local.security_group_name_id_map, "lb") ]
 }
 
 resource "aws_lb_listener" "http_listener" {
@@ -227,6 +316,24 @@ resource "aws_lb_listener" "http_listener" {
     }
 }
 
+resource "aws_lb_listener_rule" "listener_rules" {
+    for_each = local.services
+    listener_arn = aws_lb_listener.http_listener.arn
+    priority = each.value.listener_priority
+
+    action {
+        type = "forward"
+        target_group_arn = lookup(local.service_target_group_id_map, each.key)
+    }
+
+    condition {
+        http_header {
+            http_header_name = "X-Service"
+            values = [ each.key ]
+        }
+    }
+}
+
 resource "aws_lb_target_group" "lb_tgs" {
     for_each = local.services
 
@@ -235,6 +342,11 @@ resource "aws_lb_target_group" "lb_tgs" {
     protocol = "HTTP"
     target_type = "ip"
     vpc_id = aws_vpc.vpc.id
+
+    health_check {
+        enabled = true
+        path = each.value.health_path
+    }
 }
 
 resource "aws_ecs_cluster" "cluster" {
@@ -243,7 +355,7 @@ resource "aws_ecs_cluster" "cluster" {
 
     setting {
         name = "containerInsights"
-        value = "disabled"
+        value = "enabled"
     }
 
     default_capacity_provider_strategy {
@@ -255,9 +367,144 @@ resource "aws_ecr_repository" "ecr_repositories" {
     for_each = local.services
 
     name = format("%s-%s", var.org, each.key)
-    image_tag_mutability = "IMMUTABLE"
+    image_tag_mutability = "MUTABLE"
 
     tags = {
         Name = format("%s-%s", var.org, each.key)
     }    
+}
+
+data "aws_iam_policy_document" "bot_role_inline_policy" {
+    version = "2012-10-17"
+    statement {
+        actions = [
+            "ecs:*",
+            "ecr:*"
+        ]
+        effect = "Allow"
+        resources = ["*"]
+    }
+}
+
+data "aws_iam_policy_document" "bot_role_assume_policy" {
+    version = "2012-10-17"
+    statement {
+        actions = [
+            "sts:AssumeRole"
+        ]
+        effect = "Allow"
+        principals {
+            type = "Service"
+            identifiers = [
+                "ecs.amazonaws.com",
+               "ecs-tasks.amazonaws.com",
+            ]
+        }
+    }
+}
+
+resource "aws_iam_role" "bot_role" {
+    name = format("%s-bot-role", var.org)
+    assume_role_policy = data.aws_iam_policy_document.bot_role_assume_policy.json
+    force_detach_policies = true
+
+    inline_policy { 
+        name = format("%s-bot-role-inline-policy", var.org)
+        policy = data.aws_iam_policy_document.bot_role_inline_policy.json
+    }
+
+    tags = {
+        Name = format("%s-bot-role", var.org)
+    }
+}
+
+resource "aws_ecs_task_definition" "taskDefinitions" {
+    for_each = local.services
+    family = format("%s-task-definition", each.key)
+    cpu = each.value.cpu
+    memory = each.value.memory
+    network_mode = "awsvpc"
+    requires_compatibilities = ["FARGATE"]
+    task_role_arn = aws_iam_role.bot_role.arn
+    execution_role_arn = aws_iam_role.bot_role.arn
+    
+    container_definitions = jsonencode([
+        {
+            name = each.key
+            image = lookup(local.service_repository_map, each.key)
+            cpu = each.value.cpu
+            memory = each.value.memory
+            essential = each.value.essential
+            portMappings = [
+                {
+                    containerPort = each.value.containerPort
+                    hostPort = each.value.containerPort
+                }
+            ] 
+        }
+    ])
+}
+
+resource "aws_ecs_service" "ecs_services" {
+    for_each = local.services
+    name = format("%s-service", each.key)
+    cluster = aws_ecs_cluster.cluster.id
+    task_definition = lookup(local.service_task_definition_map, each.key)
+    desired_count = each.value.desired_count
+    deployment_maximum_percent = 200
+    deployment_minimum_healthy_percent = 50
+    health_check_grace_period_seconds = 60
+    launch_type = "FARGATE"
+    wait_for_steady_state = false
+
+    network_configuration {
+        subnets = local.subnets_ids
+        security_groups = [ lookup(local.security_group_name_id_map, each.key) ]
+        assign_public_ip = true
+    }
+    
+    load_balancer {
+        target_group_arn = lookup(local.service_target_group_id_map, each.key)
+        container_name = each.key
+        container_port = each.value.containerPort
+    }
+
+    lifecycle {
+       ignore_changes = [
+            task_definition
+       ]
+    }
+
+    depends_on = [
+          aws_lb_listener.http_listener
+    ]
+}
+
+resource "aws_appautoscaling_target" "ecs_services_autoscaling_groups" {
+    for_each = local.services
+
+    max_capacity = each.value.max_count
+    min_capacity = each.value.desired_count
+    resource_id = format("service/%s/%s", aws_ecs_cluster.cluster.name, format("%s-service", each.key) )
+    scalable_dimension = "ecs:service:DesiredCount"
+    service_namespace = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_services_autoscaling_policies" {
+    for_each = local.services
+    policy_type = "TargetTrackingScaling"
+    name = format("%s-%s-memory-asg", var.org, each.key)
+    resource_id = lookup(local.service_appautoscaling_target_map, each.key).resource_id
+    scalable_dimension = lookup(local.service_appautoscaling_target_map, each.key).scalable_dimension
+    service_namespace = lookup(local.service_appautoscaling_target_map, each.key).service_namespace
+
+    target_tracking_scaling_policy_configuration {
+        predefined_metric_specification {
+          predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+        }  
+          
+        target_value = 75
+        scale_in_cooldown = 300
+        scale_out_cooldown = 300
+    }
 }
